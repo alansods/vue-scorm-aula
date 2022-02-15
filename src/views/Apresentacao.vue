4e77ac9392ab7f6ba6e682e117e4ac3a7ab75aef
Alan Santos <alansods@gmail.com>
2022-02-15 13:56:44 -0300

    css

--- src/views/Apresentacao.vue
+++ src/views/Apresentacao.vue
@@ -81,7 +81,7 @@
           >
             Apresentação
           </h2>
-          <hr class="border-t-1 border-gray-300 my-10" />
+          <hr class="border-t-1 border-gray-300 my-5 md:my-10" />
         </div>
         <p class="text-2xl text-left">Olá <NomeAluno />!</p>
 
