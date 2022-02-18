86cdd3ddb507daba5cd83ddeea0c4d8cccf370ae
Alan Santos <alansods@gmail.com>
2022-02-18 14:47:43 -0300

    link videos receitas

--- src/views/aulas/aula-03/receitas/Queijadinha.vue
+++ src/views/aulas/aula-03/receitas/Queijadinha.vue
@@ -30,7 +30,7 @@
         <div class="rounded-lg">
           <div id="video-apresentacao" class="aspect-w-16 aspect-h-9">
             <youtube
-              video-id="rSgMkfjIvmw"
+              video-id="r10UVU_ZSJM"
               class="border-white border-4 shadow-xl rounded-sm"
               @ended="ended"
             />
